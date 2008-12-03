5fd7afc695ee6c4db7eb1f22afa177b1e57af750
Ryan Baumann <ryan.baumann@gmail.com>
2008-12-03 10:38:01 -0500

    Start including XSugar stuff

--- test/test_grammar.rb
+++ test/test_grammar.rb
@@ -1,6 +1,15 @@
 require 'test/unit'
 
+require 'java'
+require File.join(File.dirname(__FILE__), *%w".. xsugar-all.jar")
+
+module XSugar
+  include_package 'dk.brics.xsugar'
+end
+
 class GrammarTest < Test::Unit::TestCase
+  parser = XSugar::StylesheetParser.new
+  
   def test_stub
     assert_equal 1, 1
   end

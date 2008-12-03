22c597f3b9eee8af60a85af358ab9d01e9d00a59
Ryan Baumann <ryan.baumann@gmail.com>
2008-12-03 11:10:19 -0500

    Stub out calls into XSugar

--- test/test_grammar.rb
+++ test/test_grammar.rb
@@ -8,7 +8,11 @@ module XSugar
 end
 
 class GrammarTest < Test::Unit::TestCase
+  charset = java.nio.charset.Charset.defaultCharset.name
+  xsg_file = File.join(File.dirname(__FILE__), *%w".. epidoc.xsg")
   parser = XSugar::StylesheetParser.new
+  xsg = File.readlines(xsg_file).to_s
+  stylesheet = parser.parse(xsg, xsg_file, charset)
   
   def test_stub
     assert_equal 1, 1

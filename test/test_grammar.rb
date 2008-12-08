e3e1328043694945ae83b1bbeb87b389ad8ca5a9
Ryan Baumann <ryan.baumann@gmail.com>
2008-12-08 14:32:54 -0500

    Add gap, expansion, underdot tests

--- test/test_grammar.rb
+++ test/test_grammar.rb
@@ -19,6 +19,20 @@ class GrammarTest < Test::Unit::TestCase
     assert_equal_fragment_transform '<sic>test</sic>', 'test(!)'
   end
 
+  def test_single_dot_gap
+    assert_equal_fragment_transform '<gap extent="1"></gap>', '.'
+  end
+  
+  def test_expansion
+    # Ancient abbreviation whose resolution is known
+    # This is how CHET-C transforms it, though the spreadsheet suggests simply <expan>a<ex>b</ex></expan>
+    assert_equal_fragment_transform '<expan>a<supplied reason="abbreviation">b</supplied></expan>', 'a(b)'
+  end
+
+  def test_damage_unicode_underdot
+    assert_equal_fragment_transform '<unclear reason="damage" cert="high">a</unclear>', 'ạ'
+  end
+
   def test_bracket_gap
     assert_equal_fragment_transform 'a<supplied reason="omitted">b</supplied>c', 'a<b>c'
   end

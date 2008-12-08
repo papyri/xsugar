3c4afb682dc568c193d8df399c69ec30d7ce105e
Ryan Baumann <ryan.baumann@gmail.com>
2008-12-08 10:24:11 -0500

    Initialize parsers for testing

--- test/test_grammar.rb
+++ test/test_grammar.rb
@@ -2,17 +2,43 @@ require 'test/unit'
 
 require 'java'
 require File.join(File.dirname(__FILE__), *%w".. lib xsugar-all.jar")
+# require File.join(File.dirname(__FILE__), *%w".. lib grammar.jar")
 
 module XSugar
   include_package 'dk.brics.xsugar'
 end
 
+module XSugarXML
+  include_package 'dk.brics.xsugar.xml'
+end
+
+module XSugarParser
+  include_package 'dk.brics.grammar.parser'
+end
+
 class GrammarTest < Test::Unit::TestCase
   charset = java.nio.charset.Charset.defaultCharset.name
+  out = java.io.PrintWriter.new(java.lang.System.out, true)
+  
   xsg_file = File.join(File.dirname(__FILE__), *%w".. epidoc.xsg")
   parser = XSugar::StylesheetParser.new
   xsg = File.readlines(xsg_file).to_s
   stylesheet = parser.parse(xsg, xsg_file, charset)
+  XSugar::StylesheetChecker.new.check(stylesheet)
+  
+  grammar_builder = XSugar::GrammarBuilder.new(false)
+  grammar_builder.convert(stylesheet)
+  l_grammar = grammar_builder.getNonXMLGrammar
+  x_grammar = grammar_builder.getXMLGrammar
+  
+  normalizing_grammar_builder = XSugar::GrammarBuilder.new(true)
+  XSugarXML::StylesheetNormalizer.new.normalize(stylesheet)
+  normalizing_grammar_builder.convert(stylesheet)
+  normalized_l_grammar = normalizing_grammar_builder.getNonXMLGrammar
+  normalized_x_grammar = normalizing_grammar_builder.getXMLGrammar
+  
+  parser_l = XSugarParser::Parser.new(l_grammar, out)
+  parser_x = XSugarParser::Parser.new(normalized_x_grammar, out)
   
   def test_stub
     assert_equal 1, 1

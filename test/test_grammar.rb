904d2cff0667fb63ffae6d42f1369c04c0fdd43c
Ryan Baumann <ryan.baumann@gmail.com>
2008-12-08 11:50:39 -0500

    Move XSugar logic outside of test helper and into standalone Ruby class

--- test/test_grammar.rb
+++ test/test_grammar.rb
@@ -6,11 +6,16 @@ class GrammarTest < Test::Unit::TestCase
     # epidoc_content = File.readlines(epidoc_txt).to_s
     # puts epidoc_content
     # puts non_xml_to_xml(epidoc_content)
-    puts non_xml_to_xml("test\n")
-    puts non_xml_to_xml("test\ntest2\n")
+    puts @xsugar.non_xml_to_xml("test\n")
+    puts @xsugar.non_xml_to_xml("test\ntest2\n")
     puts ab("test")
-    puts xml_to_non_xml(non_xml_to_xml("test\n"))
-    assert_equal "test", xml_to_non_xml(non_xml_to_xml("test"))
-    assert_equal "test1\ntest2", xml_to_non_xml(non_xml_to_xml("test1\ntest2"))
+    puts @xsugar.xml_to_non_xml(@xsugar.non_xml_to_xml("test\n"))
+    assert_equal "test", @xsugar.xml_to_non_xml(@xsugar.non_xml_to_xml("test"))
+    assert_equal "test1\ntest2", @xsugar.xml_to_non_xml(@xsugar.non_xml_to_xml("test1\ntest2"))
+  end
+  
+  def test_xml_trailing_newline_stripped
+    assert_equal "test", @xsugar.xml_to_non_xml(@xsugar.non_xml_to_xml("test\n"))
+    assert_equal "test1\ntest2", @xsugar.xml_to_non_xml(@xsugar.non_xml_to_xml("test1\ntest2\n"))
   end
 end

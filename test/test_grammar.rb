bbb7a2356c1e617074476e5c1c5f914c3787c73a
Ryan Baumann <ryan.baumann@gmail.com>
2008-12-11 11:47:49 -0500

    Link to EpiDoc guidelines

--- test/test_grammar.rb
+++ test/test_grammar.rb
@@ -1,16 +1,21 @@
 require 'test_helper'
 
 class GrammarTest < Test::Unit::TestCase
+  # http://www.stoa.org/epidoc/gl/5/abbreviationsunderstood.html
   def test_expansion
     # Ancient abbreviation whose resolution is known
     assert_equal_fragment_transform 'a(b)', '<expan>a<ex>b</ex></expan>'
   end
   
+  # http://www.stoa.org/epidoc/gl/5/abbreviationsunderstood.html
   def test_symbol_expansion
     # Single symbol for an entire word
     assert_equal_fragment_transform '(abc)', '<expan><ex>abc</ex></expan>'
   end
   
+  # http://www.stoa.org/epidoc/gl/5/abbreviationsunderstood.html
+  # http://www.stoa.org/epidoc/gl/5/numbersandnumerals.html
+  # possibly: http://www.stoa.org/epidoc/gl/5/acrophonic.html
   def test_counting_symbol_expansion
     # A single symbol (one day representable in Unicode) was used to
     # indicate some number of things (usually monetary denominations)
@@ -36,16 +41,19 @@ class GrammarTest < Test::Unit::TestCase
     assert_equal_fragment_transform '(abc 123)', '<expan><ex>abc 123</ex></expan>'
   end
   
+  # http://www.stoa.org/epidoc/gl/5/abbreviationsnotunderstood.html
   def test_abbreviation_unknown_resolution
     # Ancient abbreviation whose resolution is unknown
     assert_equal_fragment_transform 'ab( )', '<abbr>ab</abbr>'
   end
   
+  # http://www.stoa.org/epidoc/gl/5/abbreviationsunderstood.html
   def test_abbreviation_uncertain_resolution
     # Ancient abbreviation whose resolution is uncertain
     assert_equal_fragment_transform '(abc?)', '<expan><ex cert="low">abc</ex></expan>'
   end
   
+  # http://www.stoa.org/epidoc/gl/5/lostcertain.html
   def test_lost_dot_gap
     # Some number of missing characters not greater than 3
     # TODO: [ca.N]
@@ -54,11 +62,14 @@ class GrammarTest < Test::Unit::TestCase
     assert_equal_fragment_transform '[.3]', '<gap reason="lost" extent="3" unit="character"></gap>'
   end
   
+  # http://www.stoa.org/epidoc/gl/5/lostcertain.html
+  # but extent="unknown" not explicitly combined with unit="character" in guidelines
   def test_lost_gap_unknown
     # Some unknown number of lost characters
     assert_equal_fragment_transform '[ca.?]', '<gap reason="lost" extent="unknown" unit="character"></gap>'
   end
   
+  # http://www.stoa.org/epidoc/gl/5/vestiges.html
   def test_illegible_dot_gap
     # Some number of illegible characters not greater than 3
     assert_equal_fragment_transform '.', '<gap reason="illegible" extent="1" unit="character"></gap>'
@@ -66,11 +77,13 @@ class GrammarTest < Test::Unit::TestCase
     assert_equal_fragment_transform '.3', '<gap reason="illegible" extent="3" unit="character"></gap>'
   end
   
+  # http://www.stoa.org/epidoc/gl/5/vestiges.html
   def test_illegible_gap_unknown
     # Some unknown number of illegible letters
     assert_equal_fragment_transform 'ca.?', '<gap reason="illegible" extent="unknown" unit="character"></gap>'
   end
   
+  # http://www.stoa.org/epidoc/gl/5/vestiges.html
   def test_illegible_gap_ca
     # Some number of illegible characters greater than 3
     assert_equal_fragment_transform 'ca.4', '<gap reason="illegible" extent="4" unit="character"></gap>'
@@ -78,6 +91,8 @@ class GrammarTest < Test::Unit::TestCase
     assert_equal_fragment_transform 'ca.10', '<gap reason="illegible" extent="10" unit="character"></gap>'
   end
   
+  # http://www.stoa.org/epidoc/gl/5/vestiges.html
+  # but no desc="vestiges"
   def test_vestige_lines
     # vestiges of N lines, mere smudges really, visible
     assert_equal_fragment_transform 'vestig.3lin', '<gap reason="illegible" extent="3" unit="line" desc="vestiges"></gap>'
@@ -85,6 +100,8 @@ class GrammarTest < Test::Unit::TestCase
     assert_equal_fragment_transform 'vestig.10lin', '<gap reason="illegible" extent="10" unit="line" desc="vestiges"></gap>'
   end
   
+  # http://www.stoa.org/epidoc/gl/5/vestiges.html
+  # but no desc="vestiges"
   def test_vestige_lines_ca
     # vestiges of rough number of lines, mere smudges really, visible
     assert_equal_fragment_transform 'vestig.ca.3lin', '<gap reason="illegible" extent="3" unit="line" precision="circa" desc="vestiges"></gap>'
@@ -92,21 +109,27 @@ class GrammarTest < Test::Unit::TestCase
     assert_equal_fragment_transform 'vestig.ca.10lin', '<gap reason="illegible" extent="10" unit="line" precision="circa" desc="vestiges"></gap>'
   end
   
+  # http://www.stoa.org/epidoc/gl/5/vestiges.html
+  # should this have desc="vestiges"?
   def test_vestige_lines_unknown
     # vestiges of an unspecified number of lines, mere smudges, visible
     assert_equal_fragment_transform 'vestig.?lin', '<gap reason="illegible" extent="unknown" unit="line"></gap>'
   end
   
+  # http://www.stoa.org/epidoc/gl/5/vestiges.html
+  # but no desc="vestiges"
   def test_vestige_characters
     # vestiges of an unspecified number of characters, mere smudges, visible
     assert_equal_fragment_transform 'vestig', '<gap reason="illegible" extent="unknown" unit="character" desc="vestiges"></gap>'
   end
   
+  # http://www.stoa.org/epidoc/gl/5/lostline.html
   def test_gap_break
     # The text breaks off completely
     assert_equal_fragment_transform 'BREAK', '<gap reason="lost" extent="unknown" unit="line"></gap>'
   end
   
+  # FIXME: reconcile with test_lost_dot_gap
   def test_lost_characters
     # Some number of characters is lost
     assert_equal_fragment_transform 'lost.3char', '<gap reason="lost" "extent="3" unit="character"></gap>'
@@ -114,11 +137,13 @@ class GrammarTest < Test::Unit::TestCase
     assert_equal_fragment_transform 'lost.10char', '<gap reason="lost" "extent="10" unit="character"></gap>'
   end
   
+  # FIXME: reconcile with test_lost_gap_unknown
   def test_lost_characters_unknown
     # Some unknown number of lost characters
     assert_equal_fragment_transform 'lost.?char', '<gap reason="lost" "extent="unknown" unit="character"></gap>'
   end
   
+  # http://www.stoa.org/epidoc/gl/5/lostline.html
   def test_lost_lines
     # Some number of lines is lost
     assert_equal_fragment_transform 'lost.3lin', '<gap reason="lost" "extent="3" unit="line"></gap>'
@@ -126,22 +151,27 @@ class GrammarTest < Test::Unit::TestCase
     assert_equal_fragment_transform 'lost.10lin', '<gap reason="lost" "extent="10" unit="line"></gap>'
   end
   
+  # http://www.stoa.org/epidoc/gl/5/lostline.html
+  # FIXME: reconcile with test_gap_break
   def test_lost_lines_unknown
     # Some unknown number of lost lines
     assert_equal_fragment_transform 'lost.?lin', '<gap reason="lost" "extent="unknown" unit="line"></gap>'
   end
   
+  # http://www.stoa.org/epidoc/gl/5/erroneousomission.html
   def test_omitted
     # Scribe omitted character(s) and modern ed inserted it
     assert_equal_fragment_transform 'a<b>c', 'a<supplied reason="omitted">b</supplied>c'
     assert_equal_fragment_transform '<abc>', '<supplied reason="omitted">abc</supplied>'
   end
   
+  # http://www.stoa.org/epidoc/gl/5/erroneousinclusion.html
   def test_sic
     # scribe wrote unnecessary characters and modern ed flagged them as such
     assert_equal_fragment_transform '{test}', '<sic>test</sic>'
   end
   
+  # http://www.stoa.org/epidoc/gl/5/supplementforlost.html
   def test_lost
     # modern ed restores lost text
     assert_equal_fragment_transform '[abc]', '<supplied reason="lost">abc</supplied>'
@@ -149,11 +179,13 @@ class GrammarTest < Test::Unit::TestCase
     assert_equal_fragment_transform 'a[bc def g]hi', 'a<supplied reason="lost">bc def g</supplied>hi'
   end
   
+  # http://www.stoa.org/epidoc/gl/5/supplementforlost.html
   def test_lost_uncertain
     # modern ed restores lost text, with less than total confidence; this proved messy to handle in IDP1
     assert_equal_fragment_transform 'a[bc?]', 'a<supplied reason="lost" cert="low">bc</supplied>'
   end
 
+  # http://www.stoa.org/epidoc/gl/5/unclear.html
   def test_unicode_underdot_unclear
     # eds read dotted letter with less than full confidence
     # TODO: handle existing cert attributes
@@ -162,16 +194,19 @@ class GrammarTest < Test::Unit::TestCase
     assert_equal_fragment_transform 'ạ', '<unclear reason="undefined">a</unclear>'
   end
   
+  # http://www.stoa.org/epidoc/gl/5/unclear.html
   def test_unicode_underdot_unclear_combining
     # eds read dotted letter with less than full confidence
     assert_equal_fragment_transform 'ạḅc̣', '<unclear reason="undefined">abc</unclear>'
   end
   
+  # http://www.stoa.org/epidoc/gl/5/deletion.html
   def test_ancient_erasure
     # ancient erasure/cancellation/expunction
     assert_equal_fragment_transform 'a[[bc]]', 'a<del rend="erasure">bc</del>'
   end
   
+  # no EpiDoc guideline, inherited from TEI
   def test_quotation_marks
     # quotation marks on papyrus
     assert_equal_fragment_transform '"abc"', '<q>abc</q>'

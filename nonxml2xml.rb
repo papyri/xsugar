c361cf1bcc5c7d5448a34fc68068141ebe5701af
Ryan Baumann <ryan.baumann@gmail.com>
2009-02-13 16:58:57 -0500

    Refactor utilities to something more DRY

--- nonxml2xml.rb
+++ nonxml2xml.rb
@@ -1,21 +1,4 @@
 #!/usr/bin/env ruby
-require File.join(File.dirname(__FILE__), *%w"lib xsugar")
+require 'util_helper'
 
-non_xml_file = STDIN
-grammar_file = RXSugarHelper::DEFAULT_GRAMMAR
-
-if ARGV.length >= 1 && ARGV[0] == '--help'
-  puts "Usage: #{$0} < input.txt > output.xml\n" +
-       "       #{$0} grammar.xsg < input.txt > output.xml\n" +
-       "       #{$0} grammar.xsg input.txt > output.xml"
-  Process.exit
-end
-
-if ARGV.length >= 1
-  grammar_file = ARGV[0]
-  if ARGV.length >= 2
-    non_xml_file = ARGV[1]
-  end
-end
-
-RXSugarHelper.non_xml_file_to_xml(non_xml_file, grammar_file)
+UtilHelper.new('txt', 'xml').main
--- xml2nonxml.rb
+++ xml2nonxml.rb
@@ -1,21 +1,4 @@
 #!/usr/bin/env ruby
-require File.join(File.dirname(__FILE__), *%w"lib xsugar")
+require 'util_helper'
 
-xml_file = STDIN
-grammar_file = RXSugarHelper::DEFAULT_GRAMMAR
-
-if ARGV.length >= 1 && ARGV[0] == '--help'
-  puts "Usage: #{$0} < input.xml > output.txt\n" +
-       "       #{$0} grammar.xsg < input.xml > output.txt\n" +
-       "       #{$0} grammar.xsg input.xml > output.txt"
-  Process.exit
-end
-
-if ARGV.length >= 1
-  grammar_file = ARGV[0]
-  if ARGV.length >= 2
-    xml_file = ARGV[1]
-  end
-end
-
-RXSugarHelper.xml_file_to_non_xml(xml_file, grammar_file)
+UtilHelper.new('xml', 'txt').main

fa1cc0fbda9cb6d32a86d9cc2162cf86e7dbfcca
Ryan Baumann <ryan.baumann@gmail.com>
2009-02-25 16:43:19 -0500

    Add JRuby pipe helper

---
+++ jruby_helper.rb
@@ -0,0 +1,29 @@
+module RXSugar
+  module JRuby
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+    
+    module ClassMethods
+      def xml2nonxml(content)
+        ruby_file = File.join(File.dirname(__FILE__), 'xml2nonxml.rb')
+        jruby_pipe(ruby_file, content)
+      end
+      
+      def nonxml2xml(content)
+        ruby_file = File.join(File.dirname(__FILE__), 'nonxml2xml.rb')
+        jruby_pipe(ruby_file, content)
+      end
+      
+      private
+      
+        def jruby_pipe(ruby_file, content)
+          IO.popen("jruby #{ruby_file}", "w+") do |pipe|
+            pipe.puts content
+            pipe.close_write
+            pipe.read
+          end
+        end
+    end
+  end
+end

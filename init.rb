9f621401cdc3b022076792085177b2dbe75c1809
Ryan Baumann <ryan.baumann@gmail.com>
2009-02-26 11:33:50 -0500

    s/Require/require/

--- init.rb
+++ init.rb
@@ -1,2 +1,3 @@
-Require File.join(File.dirname(__FILE__), *%w'lib jruby_helper')
+require File.join(File.dirname(__FILE__), *%w'lib jruby_helper')
+
 ActiveRecord::Base.send(:include, RXSugar::JRubyHelper)

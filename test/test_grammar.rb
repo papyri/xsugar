9579a0448a517d81c2fd92ace10a37d60ec3aaeb
Ryan Baumann <ryan.baumann@gmail.com>
2009-03-10 14:00:12 -0400

    add test for foreign lang

--- test/test_grammar.rb
+++ test/test_grammar.rb
@@ -314,6 +314,10 @@ if(RUBY_PLATFORM == 'java')
       assert_equal_fragment_transform '/*abcdefg*/', '<note lang="en">abcdefg</note>'
     end
     
+    def test_foreign_lang
+      assert_equal_fragment_transform '~veni vedi vici~la', '<foreign lang="la">veni vedi vici</foreign>'
+    end
+    
     def test_simple_reversibility
       assert_equal_non_xml_to_xml_to_non_xml "1. test", "1. test"
       assert_equal_non_xml_to_xml_to_non_xml "1. test1\n2. test2", "1. test1\n2. test2"

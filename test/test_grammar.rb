2500b923df57595f1fb618549356be7c5217c633
unknown <jfox@.cvve.uky.edu>
2009-06-03 16:24:21 -0400

    empty tag sup lost

--- test/test_grammar.rb
+++ test/test_grammar.rb
@@ -232,7 +232,8 @@ if(RUBY_PLATFORM == 'java')
     # http://www.stoa.org/epidoc/gl/5/supplementforlost.html
     def test_lost
       # modern ed restores lost text
-      assert_equal_fragment_transform '[abc]', '<supplied reason="lost">abc</supplied>'
+      assert_equal_fragment_transform '[]', '<supplied reason="lost"/>'
+	  assert_equal_fragment_transform '[abc]', '<supplied reason="lost">abc</supplied>'
       assert_equal_fragment_transform 'a[b]c', 'a<supplied reason="lost">b</supplied>c'
       assert_equal_fragment_transform 'a[bc def g]hi', 'a<supplied reason="lost">bc def g</supplied>hi'
     end

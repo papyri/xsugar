3f27618a0a6b424a31bbb0b5550d2bfecfaeea62
Ryan Baumann <ryan.baumann@gmail.com>
2009-09-09 10:05:51 -0400

    Bump memory limit back

--- bin/coverage.sh
+++ bin/coverage.sh
@@ -12,7 +12,7 @@ echo "xsugar git:" >> $idp2_prefix/coverage/$today.txt
 git pull >> $idp2_prefix/coverage/$today.txt
 git log -n 1 --pretty=oneline >> $idp2_prefix/coverage/$today.txt
 echo "coverage output:" >> $idp2_prefix/coverage/$today.txt
-$jruby_path -J-Xmx15G -S rake coverage:ddb DDB_DATA_PATH=$idp2_prefix/svn/idp.data/DDB_EpiDoc_XML/ SAMPLE_FRAGMENTS=10 HTML_OUTPUT=$idp2_prefix/coverage/$today.html &>> $idp2_prefix/coverage/$today.txt
+$jruby_path -J-Xmx63G -S rake coverage:ddb DDB_DATA_PATH=$idp2_prefix/svn/idp.data/DDB_EpiDoc_XML/ SAMPLE_FRAGMENTS=10 HTML_OUTPUT=$idp2_prefix/coverage/$today.html &>> $idp2_prefix/coverage/$today.txt
 scp $idp2_prefix/coverage/$today.txt halsted:public_html/idp2/coverage/automated/
 ssh halsted cp public_html/idp2/coverage/automated/$today.txt public_html/idp2/coverage/automated/latest.txt
 scp $idp2_prefix/coverage/$today.html halsted:public_html/idp2/coverage/automated/

c4ba1b040ce3c951458579b392bdcaafd8e19f31
Ryan Baumann <ryan.baumann@gmail.com>
2010-03-24 16:00:26 -0400

    Switch coverage to using Git repository instead of SVN

--- bin/coverage.sh
+++ bin/coverage.sh
@@ -4,15 +4,15 @@
 jruby_path="$HOME/source/jruby-1.3.1/bin/jruby"
 idp2_prefix="$HOME/source/idp2"
 today=`date +%Y.%m.%d`
-cd $idp2_prefix/svn/idp.data
-echo "idp.data svn:" >> $idp2_prefix/coverage/$today.txt
-svn up >> $idp2_prefix/coverage/$today.txt
+cd $idp2_prefix/idp.data
+echo "idp.data git:" >> $idp2_prefix/coverage/$today.txt
+git pull isawhttp master >> $idp2_prefix/coverage/$today.txt
 cd $idp2_prefix/xsugar
 echo "xsugar git:" >> $idp2_prefix/coverage/$today.txt
 git pull >> $idp2_prefix/coverage/$today.txt
 git log -n 1 --pretty=oneline >> $idp2_prefix/coverage/$today.txt
 echo "coverage output:" >> $idp2_prefix/coverage/$today.txt
-$jruby_path -J-Xmx63G -S rake coverage:ddb DDB_DATA_PATH=$idp2_prefix/svn/idp.data/DDB_EpiDoc_XML/ SAMPLE_FRAGMENTS=10 HTML_OUTPUT=$idp2_prefix/coverage/$today.html &>> $idp2_prefix/coverage/$today.txt
+$jruby_path -J-Xmx63G -S rake coverage:ddb DDB_DATA_PATH=$idp2_prefix/idp.data/DDB_EpiDoc_XML/ SAMPLE_FRAGMENTS=10 HTML_OUTPUT=$idp2_prefix/coverage/$today.html &>> $idp2_prefix/coverage/$today.txt
 scp $idp2_prefix/coverage/$today.txt halsted:public_html/idp2/coverage/automated/
 ssh halsted cp public_html/idp2/coverage/automated/$today.txt public_html/idp2/coverage/automated/latest.txt
 scp $idp2_prefix/coverage/$today.html halsted:public_html/idp2/coverage/automated/

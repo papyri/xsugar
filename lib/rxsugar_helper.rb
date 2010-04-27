0746de699af9c3cc49a64955d9932a25b8575782
Ryan Baumann <ryan.baumann@gmail.com>
2010-04-27 14:39:48 -0400

    Move TRANSLATION_GRAMMAR definition into RXSugarHelper module

--- lib/rxsugar_helper.rb
+++ lib/rxsugar_helper.rb
@@ -4,6 +4,7 @@ module RXSugar
   module RXSugarHelper
     XSUGAR_JAR_PATH = File.join(File.dirname(__FILE__), *%w".. lib xsugar-all.jar")
     DEFAULT_GRAMMAR = File.join(File.dirname(__FILE__), *%w".. epidoc.xsg")
+    TRANSLATION_GRAMMAR = File.join(File.dirname(__FILE__), *%w".. translation_epidoc.xsg")
 
     def rxsugar_from_grammar(grammar_file = DEFAULT_GRAMMAR)
       xsg = File.readlines(grammar_file).to_s
--- test/test_translation_helper.rb
+++ test/test_translation_helper.rb
@@ -3,8 +3,6 @@ if(RUBY_PLATFORM == 'java')
   require File.join(File.dirname(__FILE__), *%w".. lib rxsugar")
   require File.join(File.dirname(__FILE__), 'test_translation_assertions')
   
-  TRANSLATION_GRAMMAR = File.join(File.dirname(__FILE__), *%w".. translation_epidoc.xsg")
-  
   class TranslationGrammarTest < Test::Unit::TestCase
     include RXSugar::RXSugarHelper
     include TranslationGrammarAssertions

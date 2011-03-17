bb40099a72ee1b5317d5f8a30e57a9aa006fe91e
Ryan Baumann <ryan.baumann@gmail.com>
2011-03-17 15:02:19 -0400

    First pass at including XSugar and getting it included in Maven build

--- src/standalone/src/main/java/info/papyri/xsugar/standalone/XSugarStandaloneTransformer.java
+++ src/standalone/src/main/java/info/papyri/xsugar/standalone/XSugarStandaloneTransformer.java
@@ -2,6 +2,20 @@ package info.papyri.xsugar.standalone;
 
 import java.io.*;
 
+import dk.brics.grammar.Grammar;
+import dk.brics.grammar.ast.AST;
+import dk.brics.grammar.operations.GrammarChecker;
+import dk.brics.grammar.operations.Unparser;
+import dk.brics.grammar.parser.Location;
+import dk.brics.grammar.parser.ParseException;
+import dk.brics.grammar.parser.Parser;
+import dk.brics.misc.Loader;
+import dk.brics.xmlgraph.XMLGraph;
+import dk.brics.xsugar.reversibility.ReversibilityChecker;
+import dk.brics.xsugar.stylesheet.*;
+import dk.brics.xsugar.validator.*;
+import dk.brics.xsugar.xml.*;
+
 public class XSugarStandaloneTransformer
 {
   public XSugarStandaloneTransformer()
@@ -12,5 +26,7 @@ public class XSugarStandaloneTransformer
   public XSugarStandaloneTransformer(String grammar)
   {
     String charset = java.nio.charset.Charset.forName("UTF-8").name();
+    
+    dk.brics.xsugar.StylesheetParser parser = new dk.brics.xsugar.StylesheetParser();
   }
 }

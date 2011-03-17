690a5a1a792efa2bb901e4fa1493a486fa6cb45b
Ryan Baumann <ryan.baumann@gmail.com>
2011-03-17 11:13:10 -0400

    Respond to all requests, stub in params

--- src/standalone/src/main/java/info/papyri/xsugar/standalone/XSugarStandaloneServlet.java
+++ src/standalone/src/main/java/info/papyri/xsugar/standalone/XSugarStandaloneServlet.java
@@ -1,6 +1,6 @@
 package info.papyri.xsugar.standalone;
  
-import java.io.IOException;
+import java.io.*;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,11 +8,31 @@ import javax.servlet.http.HttpServletResponse;
  
 public class XSugarStandaloneServlet extends HttpServlet
 {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException
     {
         response.setContentType("text/html");
         response.setStatus(HttpServletResponse.SC_OK);
         response.getWriter().println("<h1>Hello Servlet</h1>");
         response.getWriter().println("session=" + request.getSession(true).getId());
+    }
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException
+    {
+        String param_content = request.getParameter("content");
+        String param_type = request.getParameter("type");
+        String param_direction = request.getParameter("direction");
+        
+        PrintWriter out = response.getWriter();
+        
+        response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        out.println("<html>");
+        out.println("<body>");
+        out.println("You entered \"" + param_content + "\" into the text box.");
+        out.println("</body>");
+        out.println("</html>");
     }
 }

b75307c760c0622fecee31a12c409c48fa24a3b0
Ryan Baumann <ryan.baumann@gmail.com>
2011-03-17 14:06:32 -0400

    Stub caching hashtable of transformers

--- src/standalone/src/main/java/info/papyri/xsugar/standalone/XSugarStandaloneServlet.java
+++ src/standalone/src/main/java/info/papyri/xsugar/standalone/XSugarStandaloneServlet.java
@@ -1,12 +1,11 @@
 package info.papyri.xsugar.standalone;
 
 import java.io.*;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import java.net.URL;
+import java.util.Hashtable;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
 
 import info.papyri.xsugar.standalone.XSugarStandaloneTransformer;
 
@@ -15,6 +14,27 @@ import org.apache.commons.io.IOUtils;
 
 public class XSugarStandaloneServlet extends HttpServlet
 {
+    private Hashtable transformers = null;
+    
+    public void init(ServletConfig config) throws ServletException
+    {
+        super.init(config);
+        
+        transformers = new Hashtable();
+    }
+    
+    private XSugarStandaloneTransformer getTransformer(String transformer_name)
+    {
+        XSugarStandaloneTransformer transformer = 
+            (XSugarStandaloneTransformer)transformers.get(transformer_name);
+        if (transformer == null) {
+            System.out.println("Cache miss for " + transformer_name);
+            transformer = new XSugarStandaloneTransformer();
+            transformers.put(transformer_name, transformer);
+        }
+        return transformer;
+    }
+    
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException
     {
@@ -31,12 +51,11 @@ public class XSugarStandaloneServlet extends HttpServlet
         StringWriter url_writer = new StringWriter();
         IOUtils.copy(url.openStream(), url_writer);
         
-        
-        XSugarStandaloneTransformer transformer = new XSugarStandaloneTransformer();
-        
         String param_content = request.getParameter("content");
         String param_type = request.getParameter("type");
         String param_direction = request.getParameter("direction");
+        
+        XSugarStandaloneTransformer transformer = getTransformer(param_type);
         
         PrintWriter out = response.getWriter();
         

0c826b9ee3edecafa5b374d5e4f3c33caff9c530
Ryan Baumann <ryan.baumann@gmail.com>
2011-03-18 11:38:54 -0400

    Fill in actual grammar and conversion on transformer side

--- src/standalone/src/main/java/info/papyri/xsugar/standalone/XSugarStandaloneTransformer.java
+++ src/standalone/src/main/java/info/papyri/xsugar/standalone/XSugarStandaloneTransformer.java
@@ -18,9 +18,18 @@ import dk.brics.xsugar.reversibility.ReversibilityChecker;
 import dk.brics.xsugar.stylesheet.*;
 import dk.brics.xsugar.validator.*;
 import dk.brics.xsugar.xml.*;
+import dk.brics.xsugar.*;
 
 public class XSugarStandaloneTransformer
 {
+  private Stylesheet stylesheet = null;
+  private Grammar l_grammar = null;
+  private Grammar x_grammar = null;
+  private Parser parser_l = null;
+  private Parser parser_x = null;
+  
+  private Grammar normalized_l_grammar = null;
+  
   public XSugarStandaloneTransformer()
   {
   
@@ -29,10 +38,51 @@ public class XSugarStandaloneTransformer
   public XSugarStandaloneTransformer(String grammar)
     throws dk.brics.xsugar.XSugarException, IOException, ParseException, dk.brics.relaxng.converter.ParseException, InstantiationException,	IllegalAccessException, ClassNotFoundException
   {
+    PrintWriter out = new PrintWriter(java.lang.System.out, true);
     String charset = java.nio.charset.Charset.forName("UTF-8").name();
     
-    dk.brics.xsugar.StylesheetParser parser = new dk.brics.xsugar.StylesheetParser();
+    StylesheetParser parser = new StylesheetParser();
     
     Stylesheet stylesheet = parser.parse(grammar, "dummy.xsg", charset);
+    new StylesheetChecker().check(stylesheet);
+    GrammarBuilder grammar_builder = new GrammarBuilder(false);
+    grammar_builder.convert(stylesheet);
+    
+    l_grammar = grammar_builder.getNonXMLGrammar();
+    x_grammar = grammar_builder.getXMLGrammar();
+    
+    GrammarBuilder normalizing_grammar_builder = new GrammarBuilder(true);
+    new StylesheetNormalizer().normalize(stylesheet);
+    normalizing_grammar_builder.convert(stylesheet);
+    normalized_l_grammar = normalizing_grammar_builder.getNonXMLGrammar();
+    Grammar normalized_x_grammar = normalizing_grammar_builder.getXMLGrammar();
+
+    parser_l = new Parser(l_grammar, out);
+    parser_x = new Parser(normalized_x_grammar, out);
+  }
+  
+  public String nonXMLToXML(String text)
+    throws dk.brics.grammar.parser.ParseException
+  {
+    AST ast = parser_l.parse(text, "dummy.txt");
+    
+    String output = new Unparser(x_grammar).unparse(ast);
+    output = new EndTagNameAdder().fix(output);
+    output = new NamespaceAdder(stylesheet).fix(output);
+    
+    return output;
+  }
+  
+  public String XMLToNonXML(String xml)
+    throws org.jdom.JDOMException, dk.brics.grammar.parser.ParseException, IOException
+  {
+    InputNormalizer norm = new InputNormalizer();
+    String input = norm.normalize(xml, "dummy.xml");
+    
+    AST ast = parser_x.parse(input, "dummy.xml");
+    new ASTUnescaper().unescape(ast);
+    String output = new Unparser(normalized_l_grammar).unparse(ast);
+    
+    return output;
   }
 }

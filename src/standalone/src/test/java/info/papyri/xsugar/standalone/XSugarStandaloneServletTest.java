ef8073081fe626202c31dfb0d0db294ba7557119
Ryan Baumann <ryan.baumann@gmail.com>
2011-07-19 16:53:07 -0400

    Stub some servlet tests

--- src/standalone/src/test/java/info/papyri/xsugar/standalone/XSugarStandaloneServletTest.java
+++ src/standalone/src/test/java/info/papyri/xsugar/standalone/XSugarStandaloneServletTest.java
@@ -3,11 +3,17 @@ package info.papyri.xsugar.standalone;
 import java.io.*;
 
 import org.junit.*;
+import static org.junit.Assert.*;
 import org.mortbay.jetty.testing.*;
+
+import org.apache.commons.lang.StringEscapeUtils;
 
 public class XSugarStandaloneServletTest {
   private static ServletTester tester;
   private static String baseUrl;
+
+  private static String exampleEpiDoc = "<div xml:lang=\"grc\" type=\"edition\" xml:space=\"preserve\" xmlns:xml=\"http://www.w3.org/XML/1998/namespace\">\n<ab>\n<lb n=\"1\"/>\n</ab></div>";
+  private static String exampleLeidenPlus = "<S=.grc\n<=\n1. \n=>";
 
   @BeforeClass public static void initServletContainer ()
     throws Exception
@@ -40,5 +46,30 @@ public class XSugarStandaloneServletTest {
 
     response.parse(tester.getResponses(request.generate()));
     System.out.println(response.getContent());
+  }
+
+  private void testGoodPostTransform(String input, String expected, String direction)
+    throws Exception
+  {
+    HttpTester request = new HttpTester();
+    HttpTester response = new HttpTester();
+
+    request.setMethod("POST");
+    request.setHeader("Host","tester");
+    request.setHeader("Content-Type","application/x-www-form-urlencoded");
+    request.setURI("/xsugar?content=" + java.net.URLEncoder.encode(input, "UTF-8") + "&type=epidoc&direction=" + direction);
+    request.setVersion("HTTP/1.1");
+
+    response.parse(tester.getResponses(request.generate()));
+    System.out.println(response.getContent());
+    assertEquals("{\n\"content\": \"" + StringEscapeUtils.escapeJavaScript(expected) + "\"\n}\n", response.getContent());
+  }
+
+  @Test
+    public void testPost()
+    throws Exception
+  {
+    testGoodPostTransform(exampleLeidenPlus, exampleEpiDoc, "nonxml2xml"); 
+    testGoodPostTransform(exampleEpiDoc, exampleLeidenPlus, "xml2nonxml"); 
   }
 }

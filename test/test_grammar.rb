f93ad438c04f61f6b30fc1ac8fb007dc889816b7
Ryan Baumann <ryan.baumann@gmail.com>
2015-12-03 14:03:44 -0500

    Add an additional test and explicit rule for illegible_dot to handle instances of gaps followed by ". " (fixes #1)

--- test/test_grammar.rb
+++ test/test_grammar.rb
@@ -192,6 +192,7 @@ if(RUBY_PLATFORM == 'java')
     assert_equal_fragment_transform '.3', '<gap reason="illegible" quantity="3" unit="character"/>'
     assert_equal_fragment_transform '.4. ', '<gap reason="illegible" quantity="4" unit="character"/>. '
     assert_equal_fragment_transform '8. .1. ' + "\n\n" + '9. <:κ|reg|κα̣:>', '<lb n="8"/><gap reason="illegible" quantity="1" unit="character"/>. ' + "\n\n" + '<lb n="9"/><choice><reg>κ</reg><orig>κ<unclear>α</unclear></orig></choice>'
+    assert_equal_fragment_transform '6. Μ̣ε̣ο̣.4. τοῦ ' + "\n\n" + '7. ((ἔτους)) ', '<lb n="6"/><unclear>Μεο</unclear><gap reason="illegible" quantity="4" unit="character"/>. τοῦ ' + "\n\n" + '<lb n="7"/><expan><ex>ἔτους</ex></expan> '
     (4..100).each do |n|
       assert_equal_fragment_transform ".#{n}", "<gap reason=\"illegible\" quantity=\"#{n}\" unit=\"character\"/>"
     end
